777e40fbe87515a424e33030e342a18b0bb63984
uglymushroom <lifanjin@ucla.edu>
2019-12-11 23:05:02 -0800

    fixes to bool func logic to simplify

--- problems/UglySolution1/leapTestCase.c
+++ problems/UglySolution1/leapTestCase.c
@@ -5,7 +5,7 @@
 
 bool isLeapYear(int year)
 {
-    if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0))
+    if ((year % 400 == 0))
         return true;
     else if ((year % 4 == 0) && (year % 100 == 0))
         return false;
